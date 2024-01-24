2566a94a52eed3b81c45bc3bae207ee4923c4310
dbvsf <darlan_bueno@hotmail.com>
2024-01-23 21:04:43 -0300

    Adição: Validação dos formulários e navegação entre as telas.

--- lib/screens/initial_screen.dart
+++ lib/screens/initial_screen.dart
@@ -1,5 +1,8 @@
+
+
 import 'package:flutter/material.dart';
 import 'package:primeiro_projeto/components/task.dart';
+import 'package:primeiro_projeto/screens/form_screen.dart';
 
 class InitialScreen extends StatefulWidget {
   const InitialScreen({super.key});
@@ -9,8 +12,6 @@ class InitialScreen extends StatefulWidget {
 }
 
 class _InitialScreenState extends State<InitialScreen> {
-  bool isOpacity = true;
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,28 +19,27 @@ class _InitialScreenState extends State<InitialScreen> {
         backgroundColor: Colors.lightBlueAccent,
         title: const Text('Tarefas'),
       ),
-      body: AnimatedOpacity(
-        opacity: isOpacity ? 1 : 0,
-        duration: const Duration(milliseconds: 800),
-        child: ListView(
-          children: const [
-            Task('Aprender Flutter', 'assets/images/flutter.png', 4),
-            Task('Caminhar', 'assets/images/caminhar.jpeg', 5),
-            Task('Estudar', 'assets/images/estudar.png', 3),
-            Task('Jogar', 'assets/images/jogar.jpg', 1),
-            SizedBox(
-              height: 80,
-            ),
-          ],
-        ),
+      body: ListView(
+        children: const [
+          Task('Aprender Flutter', 'assets/images/flutter.png', 4),
+          Task('Caminhar', 'assets/images/caminhar.jpeg', 5),
+          Task('Estudar', 'assets/images/estudar.png', 3),
+          Task('Jogar', 'assets/images/jogar.jpg', 1),
+          SizedBox(
+            height: 80,
+          ),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          setState(() {
-            isOpacity = !isOpacity;
-          });
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => FormScreen(),
+            ),
+          );
         },
-        child: const Icon(Icons.remove_circle),
+        child: const Icon(Icons.add_circle_outline),
       ),
     );
   }
